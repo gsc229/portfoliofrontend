c681c36d55d05708fbd15e31fa1893c74ad90f51
greg <gsc229@gmail.com>
2020-05-26 17:44:05 -0400

    removed active status on sidebar

--- portfolio/src/components/SideBar.js
+++ portfolio/src/components/SideBar.js
@@ -40,7 +40,7 @@ const SidebarExampleSidebar = ({visible, setVisible, props}) => {
               as='a' 
               name='resume'
               id='resume'
-              active={activeItem === 'resume'} 
+              
               onClick={handleItemClick}
               href='https://resume.io/r/l8u6bawut' 
               >
@@ -55,10 +55,10 @@ const SidebarExampleSidebar = ({visible, setVisible, props}) => {
               duration={800}   
               name='contact'
               id='contact'
-              active={activeItem === 'contact'}
+              
               onClick={contactClick}
               >
-                <FontAwesomeIcon  className='sidebar-icon' icon={['far', 'paper-plane']} />&nbsp;
+                <FontAwesomeIcon className='sidebar-icon' icon={['far', 'paper-plane']} />&nbsp;
                 <br/><br/><p>Contact</p>
             </Menu.Item>
 
@@ -67,7 +67,7 @@ const SidebarExampleSidebar = ({visible, setVisible, props}) => {
               href='https://www.linkedin.com/in/greg-cameron-90488b30/' 
               name='linked-in'
               id='linked-in'
-              active={activeItem === 'linked-in'}
+              
               onClick={handleItemClick}
               >
                 <FontAwesomeIcon className='sidebar-icon social'  icon={['fab', 'linkedin']} />
@@ -78,7 +78,7 @@ const SidebarExampleSidebar = ({visible, setVisible, props}) => {
               href='https://github.com/gsc229' 
               name='git-hub'
               id='git-hub'
-              active={activeItem === 'git-hub'}
+              
               onClick={handleItemClick}
               >
                 <FontAwesomeIcon className='sidebar-icon social'  icon={['fab', 'github-square']} />
