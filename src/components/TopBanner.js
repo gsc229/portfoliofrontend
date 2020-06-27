0f5c67f4628c02322f0f3da90a089636572139b8
greg <gsc229@gmail.com>
2020-06-26 20:45:06 -0400

    removed un-needed space between 'my' and 'Portfolio'

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -20,7 +20,7 @@ export default function FeaturedProjects({featured_projects, visible, setVisible
 						<h1>Hi. I'm <strong>Greg Cameron</strong>.</h1>
 					</Header>
           <p>
-          And this is my &nbsp;
+          And this is my 
           <strong>
           <Item
           className="scrolly" 
